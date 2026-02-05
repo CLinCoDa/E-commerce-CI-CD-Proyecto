b5c8e4bb963c24fed9068949fed4e2f10ba0cd7c
William Varela <williamariel21@gmail.com>
2026-02-05 15:48:14 -0500

    feat: configuracion completa del flujo CI/CD - GitHub Actions + Docker

---
+++ Client/src/Helper/Axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const axiosFetch = async ({ url, method, data = null }) => {
+  //api to fetch data from postman mock server
+  try {
+    // axios.get("dsa", {});
+    console.log("error");
+    // const token = JSON.parse(sessionStorage.getItem("user") ?? "{}").token;
+    const token = sessionStorage.getItem("token") ?? "{}";
+    console.log(token);
+    const response = await axios.request({
+      url: "http://localhost:9090/" + url,
+      method,
+      data: data,
+      headers: {
+        Authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+    return response;
+  } catch (err) {
+    console.error("Error en la petición:", err);
+    // Devolvemos un objeto con data vacío para que el .map() no explote
+    return { data: [] }; 
+  }
+};
+
+export default axiosFetch;
